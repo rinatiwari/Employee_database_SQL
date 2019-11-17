849d47c234df207743fb42bcce94518e2c97978d
Rina_Tiwari <rinatiw@gmail.com>
2019-11-16 20:01:49 -0500

    first

---
+++ QuickDBD-EMPLOYEE_Ddiagram_schema.sql
@@ -0,0 +1,86 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/2fnIQ0
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Employees" (
+    "EmpNo" int   NOT NULL,
+    "Birthdate" date   NOT NULL,
+    "FirstName" varchar(14)   NOT NULL,
+    "LastName" varchar(16)   NOT NULL,
+    "Gender" varchar(1)   NOT NULL,
+    "HireDate" date   NOT NULL,
+    CONSTRAINT "pk_Employees" PRIMARY KEY (
+        "EmpNo"
+     )
+);
+
+CREATE TABLE "DepartmentEmp" (
+    "EmpNo" int   NOT NULL,
+    "DeptNo" varchar(4)   NOT NULL,
+    "FromDate" date   NOT NULL,
+    "ToDate" date   NOT NULL
+);
+
+CREATE TABLE "DepartmentManager" (
+    "DeptNo" varchar(4)fk   NOT NULL,
+    "EmpNo" int   NOT NULL,
+    "FromDate" date   NOT NULL,
+    "ToDate" date   NOT NULL
+);
+
+CREATE TABLE "Departments" (
+    "DeptNo" int   NOT NULL,
+    "DeptName" varchar(40)unique   NOT NULL,
+    CONSTRAINT "pk_Departments" PRIMARY KEY (
+        "DeptNo"
+     )
+);
+
+CREATE TABLE "Titles" (
+    "EmpNo" int   NOT NULL,
+    "Title" varchar(50)   NOT NULL,
+    "FromDate" date   NOT NULL,
+    "ToDate" date   NOT NULL
+);
+
+CREATE TABLE "Salaries" (
+    "EmpNo" int   NOT NULL,
+    "Salary" int   NOT NULL,
+    "FromDate" date   NOT NULL,
+    "ToDate" date   NOT NULL
+);
+
+ALTER TABLE "DepartmentEmp" ADD CONSTRAINT "fk_DepartmentEmp_EmpNo" FOREIGN KEY("EmpNo")
+REFERENCES "Employees" ("EmpNo");
+
+ALTER TABLE "DepartmentEmp" ADD CONSTRAINT "fk_DepartmentEmp_DeptNo" FOREIGN KEY("DeptNo")
+REFERENCES "Departments" ("DeptNo");
+
+ALTER TABLE "DepartmentEmp" ADD CONSTRAINT "fk_DepartmentEmp_FromDate" FOREIGN KEY("FromDate")
+REFERENCES "Salaries" ("FromDate");
+
+ALTER TABLE "DepartmentEmp" ADD CONSTRAINT "fk_DepartmentEmp_ToDate" FOREIGN KEY("ToDate")
+REFERENCES "DepartmentManager" ("ToDate");
+
+ALTER TABLE "DepartmentManager" ADD CONSTRAINT "fk_DepartmentManager_DeptNo" FOREIGN KEY("DeptNo")
+REFERENCES "Departments" ("DeptNo");
+
+ALTER TABLE "DepartmentManager" ADD CONSTRAINT "fk_DepartmentManager_EmpNo" FOREIGN KEY("EmpNo")
+REFERENCES "Employees" ("EmpNo");
+
+ALTER TABLE "DepartmentManager" ADD CONSTRAINT "fk_DepartmentManager_FromDate" FOREIGN KEY("FromDate")
+REFERENCES "Salaries" ("FromDate");
+
+ALTER TABLE "Titles" ADD CONSTRAINT "fk_Titles_EmpNo" FOREIGN KEY("EmpNo")
+REFERENCES "Employees" ("EmpNo");
+
+ALTER TABLE "Titles" ADD CONSTRAINT "fk_Titles_FromDate" FOREIGN KEY("FromDate")
+REFERENCES "DepartmentEmp" ("FromDate");
+
+ALTER TABLE "Titles" ADD CONSTRAINT "fk_Titles_ToDate" FOREIGN KEY("ToDate")
+REFERENCES "Salaries" ("ToDate");
+
+ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_EmpNo" FOREIGN KEY("EmpNo")
+REFERENCES "Employees" ("EmpNo");
+
---
+++ employees_schema.sql
@@ -0,0 +1,73 @@
+--Create Tables in database EmployeeSQL
+-- Create new table to import data
+
+CREATE TABLE employees (
+    emp_no      INT             NOT NULL,
+    birth_date  DATE            NOT NULL,
+    first_name  VARCHAR(14)     NOT NULL,
+    last_name   VARCHAR(16)     NOT NULL,
+    gender      VARCHAR(1) 		NOT NULL,
+    hire_date   DATE            NOT NULL,
+    PRIMARY KEY (emp_no)
+);
+
+
+CREATE TABLE departments (
+    dept_no     VARCHAR(4)         NOT NULL,
+    dept_name   VARCHAR(40)     NOT NULL,
+    PRIMARY KEY (dept_no),
+    UNIQUE   	(dept_name)
+);
+
+
+CREATE TABLE dept_manager (
+   dept_no      VARCHAR(4)         NOT NULL,
+   emp_no       INT             NOT NULL,
+   from_date    DATE            NOT NULL,
+   to_date      DATE            NOT NULL,
+   FOREIGN KEY (emp_no)  REFERENCES employees (emp_no),
+   FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+   PRIMARY KEY (emp_no,dept_no)
+);
+
+
+CREATE TABLE dept_emp (
+    emp_no      INT             NOT NULL,
+    dept_no     VARCHAR(4)         NOT NULL,
+    from_date   DATE            NOT NULL,
+    to_date     DATE            NOT NULL,
+    FOREIGN KEY (emp_no)  REFERENCES employees   (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no,dept_no)
+);
+
+
+CREATE TABLE titles (
+    emp_no      INT             NOT NULL,
+    title       VARCHAR(50)     NOT NULL,
+    from_date   DATE            NOT NULL,
+    to_date     DATE,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    PRIMARY KEY (emp_no,title, from_date)
+);
+
+
+CREATE TABLE salaries (
+    emp_no      INT             NOT NULL,
+    salary      INT             NOT NULL,
+    from_date   DATE            NOT NULL,
+    to_date     DATE            NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    PRIMARY KEY (emp_no, from_date)
+);
+
+--View Tables
+
+-- Import data from departments.csv
+-- View the table to ensure all data has been imported correctly
+SELECT * FROM employees ;
+SELECT * FROM departments ;
+SELECT * FROM dept_emp ;
+SELECT * FROM dept_manager ;
+SELECT * FROM salaries ;
+SELECT * FROM titles ;
